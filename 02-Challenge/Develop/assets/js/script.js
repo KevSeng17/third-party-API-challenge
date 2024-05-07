4bc889dfb11a7f8342fdaaffd8e7b633795db05f
Ethan <ethansengpaseuth@gmail.com>
2024-05-06 21:17:26 -0600

    few changes made

--- 02-Challenge/Develop/assets/js/script.js
+++ 02-Challenge/Develop/assets/js/script.js
@@ -4,35 +4,38 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-    const uniqueId = Math.random().toString(36).substring(2, 9);
+  const uniqueId = Math.random().toString(36).substring(2, 9);
 }
 
 // Todo: create a function to create a task card
-function createTaskCard(task) {
-
+function createTaskCard(title, description, deadline) {
+  const card = document.createElement("div");
+  card.classList.add("task-card");
+  const elementTitle = document.createElement("h3");
+  elementTitle.textContent = title;
+  const description = document.createElement("p");
+  description.textContent = description;
+  const deadline = document.createElement("p");
+  deadline.textContent = `Deadline: ${deadline}`;
+  card.addEventListener('click', () => {
+    card.remove();
+  });
+  card.appendChild(elementTitle);
+  card.appendChild(description);
+  card.appendChild(deadline);
 }
 
 // Todo: create a function to render the task list and make cards draggable
-function renderTaskList() {
-
-}
+function renderTaskList() {}
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
-
-}
+function handleAddTask(event) {}
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
-
-}
+function handleDeleteTask(event) {}
 
 // Todo: create a function to handle dropping a task into a new status lane
-function handleDrop(event, ui) {
-
-}
+function handleDrop(event, ui) {}
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
-$(document).ready(function () {
-
-});
+$(document).ready(function () {});
